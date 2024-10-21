2ffd6b5c99c6b1b599728ae76ba02f33871c196c
natasa-zupanski <zupansnm@rose-hulman.edu>
2024-10-21 10:27:20 -0400

    many equations in, hibernate and brth implemented

--- main.java
+++ main.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 class Main {
 
@@ -14,7 +13,7 @@ class Main {
 
     }
 
-    public void run() {
+    public void run(int days_to_run) {
 
     }
 
@@ -22,7 +21,7 @@ class Main {
         protected int initialLlamas = 466667;
         protected double birth_multiplier = 0;
         protected double hibernate_multiplier = 1;
-        protected double llama_growth_rate = 0;
+        protected double llama_growth_rate = 0.28;
         protected double birth_energy = 0; // ncluding fire and reproduction
         protected double max_weight_loss_hibernate = 0.2;
         protected int max_hibernate_day = 365 * 1;
@@ -34,7 +33,28 @@ class Main {
 
     protected class Dragon {
         double weight = 10; // in kg
-        int birth_age = 0; // in days
+        int birth = 0; // in days
+        double weight_at_start_hibernate = 10;
+
+        public Dragon(int birth) {
+            this.birth = birth;
+        }
+
+        public void startHibernate() {
+            weight_at_start_hibernate = weight;
+        }
+
+        public boolean isDeadAfterHibernate() {
+            if (weight < (1 - constants.max_weight_loss_hibernate) * weight_at_start_hibernate) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public int getAge() {
+            return days - birth;
+        }
     }
 
     protected class Location {
@@ -47,7 +67,7 @@ class Main {
         for (ArrayList<Dragon> dragons : dragonsAtLocations.keySet()) {
             Location current_location = dragonsAtLocations.get(dragons);
             runDayFor(dragons, current_location);
-
+            days++;
         }
     }
 
@@ -67,8 +87,14 @@ class Main {
             // first check that we can still hibernate
             if (current_location.hibernate_day > constants.max_hibernate_day) {
                 current_location.hibernate = false;
-                runDayFor(dragons, current_location); // TODO: add a check that there is enough energy not to just hibernate again, else die
-                // TODO: adjust dragon population based on who died during hibernation.
+
+                for (Dragon dragon : dragons) {
+                    if (dragon.isDeadAfterHibernate()) {
+                        dragons.remove(dragon);
+                    }
+                }
+
+                runDayFor(dragons, current_location);
                 return;
             }
         }
@@ -89,7 +115,7 @@ class Main {
                 // convenience
                 // do birth
                 dragonsAtLocations.remove(dragons);
-                dragons.add(new Dragon());
+                dragons.add(new Dragon(days));
 
                 // calculate llamas needed to spend energy to birth & bathe in fire
                 // adjust llamas to eat
@@ -104,62 +130,31 @@ class Main {
         current_location.llamas = new_llama_population;
 
         // adjust dragon weights
-        for (int i = 0; i < dragons.size(); i++) {
-            adjustWeight(dragons.get(i), energies.get(i), hibernate); // TODO: consider how to deal with case of new
-                                                                      // born dragon in here, ignore?
+        for (int i = 0; i < energies.size(); i++) {
+            adjustWeight(dragons.get(i), energies.get(i), hibernate);
         }
 
         // adjust location
-        if (hibernate)
+        if (hibernate) {
+            current_location.hibernate_day += 1;
+        }
 
-            // save changes
-            dragonsAtLocations.put(dragons, current_location);
+        // save changes
+        dragonsAtLocations.put(dragons, current_location);
     }
 
     public void adjustWeight(Dragon dragon, double energy, boolean hibernate) {
+        if (hibernate) {
+            double prop_for_growth = getProportionEnergyToGrowth(dragon);
+            dragon.weight = getNewWeight(dragon, prop_for_growth * energy);
+        } else {
+
+        }
 
     }
-
-    public void runDayHibernateFor(Set<Dragon> dragons, Location current_location) {
-
-    }
-
-    // public void runDayActiveFor(Set<Dragon> dragons, Location current_location) {
-    // double sum_energy = dragons.stream().map(dragon -> getEnergyForDay(dragon))
-    // .reduce(0.0,
-    // (Double subtotal, Double element) -> {
-    // return subtotal + element;
-    // });
-    // int llamas_to_eat = getLlamasForEnergy(sum_energy);
-    // int current_llamas = current_location.llamas;
-
-    // if (current_llamas <= constants.hibernate_multiplier * llamas_to_eat) {
-    // // hibernate
-
-    // return;
-    // }
-
-    // if (current_llamas > constants.birth_multiplier * llamas_to_eat) { // assume
-    // dragons hatch right away for
-    // // convenience
-    // // do birth
-    // dragonsAtLocations.remove(dragons);
-    // dragons.add(new Dragon());
-
-    // // calculate llamas needed to spend energy to birth & bathe in fire
-    // // adjust llamas to eat
-    // llamas_to_eat += getLlamasForEnergy(constants.birth_energy);
-    // }
-
-    // int new_llama_population = (int) Math
-    // .floor(current_llamas - llamas_to_eat + constants.llama_growth_rate *
-    // current_llamas);
-    // current_location.llamas = new_llama_population;
-    // dragonsAtLocations.put(dragons, current_location);
-    // }
 
     public int getLlamasForEnergy(Double energy) {
-        return 0;
+        return (int) Math.ceil(energy / 248800);
     }
 
     public double getNewWeight(Dragon dragon, double energy) {
@@ -167,15 +162,25 @@ class Main {
     }
 
     public double getProportionEnergyToGrowth(Dragon dragon) {
-        return 0;
+        return ((46.4 / (1 + Math.exp(-1 / (200 * (dragon.getAge() - 365))))) + 56.4) / 100;
     }
 
     public double getEnergyForDay(Dragon dragon, boolean hibernate) {
-        return 0;
+        if (!hibernate) {
+            double total_no_growth = 21 * getMetabolicEnergyPerHourStandard(dragon) + 3 * getEnergyForFlying(dragon);
+            double prop_for_growth = getProportionEnergyToGrowth(dragon);
+            return (1 / (1 - prop_for_growth)) * total_no_growth;
+        } else {
+            return 0.012 * 24 * getMetabolicEnergyPerHourStandard(dragon);
+        }
+    }
+
+    public double getMetabolicEnergyPerHourStandard(Dragon dragon) {
+        return Math.pow(10, (Math.log10(3.1) + 0.744 * Math.log10(dragon.weight)));
     }
 
     public double getEnergyForFlying(Dragon dragon) {
-        return 0;
+        return Math.pow(10, Math.log10(37.152) + 0.744 * Math.log10(dragon.weight));
     }
 
 }
